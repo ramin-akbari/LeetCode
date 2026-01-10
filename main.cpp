3117a4cd202e475a8b67a874ea4e984b5537193f
Ramin Akbari <akbarigh@msu.edu>
2026-01-10 01:29:34 -0500

    hash table solution - graph approach

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <print>
 #include "problems/threesum.cpp"
 
+
 int main() {
-    std::vector<int> test = {1,-1,0,1,2};
-    for (const auto &v:ThreeSum::twoPtr(test))
-        std::println("{:<5d},{:<5d},{:<5d}",v[0],v[1],v[2]);
+    std::vector<int> test = {2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10};
+    for (const auto &v: ThreeSum::hashMap(test))
+        std::println("{:<5d},{:<5d},{:<5d}", v[0], v[1], v[2]);
+
 }
--- problems/threesum.cpp
+++ problems/threesum.cpp
@@ -1,5 +1,8 @@
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <ranges>
 
 namespace ThreeSum {
     std::vector<std::vector<int> > twoPtr(std::vector<int> &nums) {
@@ -24,6 +27,40 @@ namespace ThreeSum {
             ++idx;
             while (idx < nums.size() && nums[idx] == target) ++idx;
         }
+        return output;
+    }
+
+    std::vector<std::vector<int> > hashMap(std::vector<int> &nums) {
+        std::vector<std::vector<int> > output;
+        std::unordered_map<int, std::unordered_set<int> > bucket;
+        std::unordered_map<int, int> finder;
+        for (const auto num: nums) ++finder[num];
+        for (int i = 0; i < nums.size(); ++i) {
+            if (bucket.contains(nums[i])) continue;
+            int target = nums[i];
+            auto &targetBucket = bucket[target];
+
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (targetBucket.contains(nums[j])) continue;
+
+                auto bucketIt = bucket.find(nums[j]);
+                if (bucketIt != bucket.end() && bucketIt->second.contains(target)) continue;
+
+                const int sumTwo = -(nums[i] + nums[j]);
+
+                bucketIt = bucket.find(sumTwo);
+                if (bucketIt != bucket.end() && bucketIt->second.contains(target)) continue;
+
+                auto it = finder.find(sumTwo);
+                if (it == finder.end()) continue;
+                if (it->second <= (static_cast<int>(sumTwo == nums[j]) + static_cast<int>(sumTwo == target))) continue;
+
+                output.push_back({target, nums[j], sumTwo});
+                targetBucket.insert({sumTwo, nums[j]});
+            }
+        }
+
+
         return output;
     }
 };
