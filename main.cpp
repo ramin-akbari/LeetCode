334ce03f3bb5f7745e31bba3ffc970e4bd2ef332
Ramin Akbari <akbarigh@msu.edu>
2026-01-09 19:55:16 -0500

    forgot to sort

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "problems/threesum.cpp"
 
 int main() {
-    std::vector<int> test = {1,-1,0,1,2,-1,-2,4,-2};
+    std::vector<int> test = {1,-1,0,1,2};
     for (const auto &v:ThreeSum::twoPtr(test))
         std::println("{:<5d},{:<5d},{:<5d}",v[0],v[1],v[2]);
 }
--- problems/threesum.cpp
+++ problems/threesum.cpp
@@ -1,32 +1,29 @@
 #include <vector>
+#include <algorithm>
 
 namespace ThreeSum {
     std::vector<std::vector<int> > twoPtr(std::vector<int> &nums) {
+        std::ranges::sort(nums);
         std::vector<std::vector<int> > output;
         int idx = 0;
         while (idx < nums.size()) {
             const int target = nums[idx];
             int left = idx + 1;
-            int right = nums.size() - 1;
+            int right = static_cast<int>(nums.size()) - 1;
 
             while (left < right) {
-                int sumOfAll = target + nums[left] + nums[right];
+                const int sumOfAll = target + nums[left] + nums[right];
                 if (!sumOfAll) {
                     output.push_back({target, nums[left++], nums[right--]});
                     while (left < right && nums[left] == nums[left - 1]) ++left;
                     continue;
                 }
-
                 if (sumOfAll > 0) --right;
                 else ++left;
             }
-
-
             ++idx;
             while (idx < nums.size() && nums[idx] == target) ++idx;
         }
-
-
         return output;
     }
 };
